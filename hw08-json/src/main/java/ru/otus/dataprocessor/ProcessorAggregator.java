21ff145d432140d9498b610c6c7b801cb752c72f
Alyona <alyonabe@yandex.ru>
2021-07-27 00:00:50 +0300

    HW08_version1

--- hw08-json/src/main/java/ru/otus/dataprocessor/ProcessorAggregator.java
+++ hw08-json/src/main/java/ru/otus/dataprocessor/ProcessorAggregator.java
@@ -2,14 +2,22 @@ package ru.otus.dataprocessor;
 
 import ru.otus.model.Measurement;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class ProcessorAggregator implements Processor {
 
+    Map<String, Double> groupedMeasurements = new TreeMap<>();
+
     @Override
     public Map<String, Double> process(List<Measurement> data) {
+        groupedMeasurements = data
+                .stream()
+                .collect(Collectors.groupingBy(Measurement::getName, Collectors.summingDouble(Measurement::getValue)));
+
         //группирует выходящий список по name, при этом суммирует поля value
-        return null;
+        System.out.println("Measurements after grouping" + groupedMeasurements);
+        groupedMeasurements = new TreeMap<String, Double>(groupedMeasurements);
+        return groupedMeasurements;
     }
 }
